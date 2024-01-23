2cf544907603ed9baa49c12e925d828c036f3115
Matthew Holmes <matthewholmes@Matthews-MBP.lan>
2024-01-23 20:38:22 +0000

    Basic Web Structure

---
+++ src/components/Links.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Links extends Component {
+  render() {
+    return (
+      <div>
+        <a>Home</a>
+        <a>Contacts</a>
+        <a>Main</a>
+      </div>
+    );
+  }
+}
+
+export default Links;
